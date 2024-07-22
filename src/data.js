07c83e6c484a61e5ba4493918a370189f96c3f1a
kavyaFullstackDeveloper <kavya.why@gmail.com>
2024-07-22 14:07:02 +0530

    vite api key

--- src/data.js
+++ src/data.js
@@ -1,6 +1,5 @@
-
-
-export const API_KEY = process.env.REACT_APP_API_KEY;
+export const API_KEY = import.meta.env.VITE_API_KEY;
+console.log('api_key:', API_KEY)
 
 export const value_converter = (value) => {
     if (value >= 1000000) {
